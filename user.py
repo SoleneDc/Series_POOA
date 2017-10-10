0872fc6ca0ce2f46197d51b3a2eb30a0227b8d10
timot <timothee.guez@student.ecp.fr>
2017-10-10 14:59:44 +0200

    première définition de User

---
+++ user.py
@@ -0,0 +1,23 @@
+class User:
+    """This class represent the user of our app"""
+
+    def __init__(self, id, firstName, lastName, birthDate, gender, picture):
+        """Basic constructor"""
+        self._id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthDate = birthDate
+        self.gender = gender
+        self.picture = picture
+
+    def _get_id(self):
+        """Getter for the id"""
+        return self._id
+
+    def _set_id(self, id):
+        """Setter for the id"""
+        print("Sorry but the id is definitive.")
+
+    #Like that the id cannot be changed
+    id = property(_get_id, _set_id)
+
